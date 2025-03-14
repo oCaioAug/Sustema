93eb6c7ecb23158a95657953826b3941e25f8333
oCaioAug <aguacalg@gmail.com>
2025-03-13 21:21:14 -0300

    Cria ApplicationDbContext

---
+++ backend/Sustema.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sustema.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+
+    }
+}
