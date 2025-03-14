a320929cff59d22d588bc4b6b37202633872b387
oCaioAug <aguacalg@gmail.com>
2025-03-13 22:03:34 -0300

    Cria UserController

---
+++ backend/Sustema.Api/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sustema.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Sustema.Api.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Sustema.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IConfiguration _configuration;
+
+        public UserController(IUserRepository userRepository, IConfiguration configuration)
+        {
+            _userRepository = userRepository;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] User user)
+        {
+            user.DataCadastro = DateTime.UtcNow;
+            await _userRepository.AddAsync(user);
+            await _userRepository.SaveChangesAsync();
+
+            return Ok(new {message = "Usuário cadastrado com sucesso" });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] User loginRequest)
+        {
+            var user = await _userRepository.GetUserByEmailAsync(loginRequest.Email);
+            if (user == null)
+            {
+                return Unauthorized(new { message = "Usuário não encontrado!" });
+            }
+
+            //Validação da senha e geração do token JWT
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var key = Encoding.ASCII.GetBytes(jwtSettings["Secret"]);
+            //var key = Encoding.ASCII.GetBytes(jwtSettings.GetSection("Secret").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Role, user.Perfil)
+                }),
+                //Expires = DateTime.UtcNow.AddHours(2),
+                Expires = DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["ExpiryMinutes"])),
+                Issuer = jwtSettings["Issuer"],
+                Audience = jwtSettings["Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new { token = tokenHandler.WriteToken(token)});
+        }
+    }
+}
