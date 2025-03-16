1c5af21fcb9826f7688eb3adcb7ac6143b18bbc9
oCaioAug <aguacalg@gmail.com>
2025-03-16 00:34:08 -0300

    Cria DTOs LoginRequest e RegisterRequest

---
+++ backend/Sustema.Api/Models/DTOs/RegisterRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sustema.Api.Models.DTOs
+{
+    public class RegisterRequest
+    {
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public PerfilUsuario Perfil { get; set; }
+
+    }
+}
