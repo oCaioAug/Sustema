07acf454062d6d819fc9a938c709f8cad1b97b98
oCaioAug <ocaioaug.dev@protonmail.com>
2025-03-19 16:21:31 -0300

    Cria RefreshToken

---
+++ backend/Sustema.Api/Models/RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿namespace Sustema.Api.Models
+{
+    /// <summary>
+    /// Refresh Token
+    /// </summary>
+    public class RefreshToken
+    {
+        /// <summary>
+        /// Token
+        /// </summary>
+        public string Token { get; set; }
+        /// <summary>
+        /// Data e hora de expiração
+        /// </summary>
+        public DateTime Expiration { get; set; }
+    }
+}
