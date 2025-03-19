249db12587dcfceb2d89056bb905b9e622fcb673
oCaioAug <ocaioaug.dev@protonmail.com>
2025-03-19 16:20:03 -0300

    Cria UserService

---
+++ backend/Sustema.Api/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Sustema.Api.Interfaces.Services;
+using Sustema.Api.Models.DTOs;
+using Sustema.Api.Repositories;
+
+namespace Sustema.Api.Services
+{
+    /// <summary>
+    /// Service para Usuário
+    /// </summary>
+    public class UserService : IUserService
+    {
+        /// <summary>
+        /// Repositorio para Usuarios
+        /// </summary>
+        public readonly IUserRepository _userRepository;
+
+        /// <summary>
+        /// Construtor da classe
+        /// </summary>
+        /// <param name="userRepository"></param>
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        /// <summary>
+        /// Busca Usuário pelo Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<UserDto> GetUserByIdAsync(int id)
+        {
+            var user = await _userRepository.GetByIdAsync(id);
+
+            return user == null ? null : new UserDto { Id = user.UserId, Nome = user.Nome, Email = user.Email, Perfil = user.Perfil};
+        }
+    }
+}
