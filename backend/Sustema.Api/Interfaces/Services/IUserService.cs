ea2740ec8fd688e8778aa0a8fe45c9c8a35b853a
oCaioAug <ocaioaug.dev@protonmail.com>
2025-03-19 16:21:04 -0300

    Criar IUserService

---
+++ backend/Sustema.Api/Interfaces/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Sustema.Api.Models.DTOs;
+
+namespace Sustema.Api.Interfaces.Services
+{
+    /// <summary>
+    /// Interface para Service Usuario
+    /// </summary>
+    public interface IUserService
+    {
+        /// <summary>
+        /// Busca um usuário pelo Id
+        /// </summary>
+        /// <param name="id">Id do usuario</param>
+        /// <returns>UserDto usuario</returns>
+        Task<UserDto> GetUserByIdAsync(int id);
+    }
+}
