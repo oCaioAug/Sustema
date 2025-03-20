ded0bc8beeb5fc260c0fdc5af95a0c3b1c264dcd
oCaioAug <ocaioaug.dev@protonmail.com>
2025-03-19 23:54:51 -0300

    Cria UpdateCollectinoPointDto

---
+++ backend/Sustema.Api/Models/DTOs/UpdateCollectionPointDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Sustema.Api.Models.DTOs
+{
+    /// <summary>
+    /// DTO para atualizacao de um ponto de coleta
+    /// </summary>
+    public class UpdateCollectionPointDto
+    {
+        public string Nome { get; set; }
+
+        public string Endereco { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
